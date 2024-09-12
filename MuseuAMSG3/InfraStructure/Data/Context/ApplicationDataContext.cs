25bdee8849a0024ae1eb96dee4de353f67dc56b5
rmelo21 <ricardo_pdf@hotmail.com>
2024-09-12 18:40:51 -0300

    Adicionar arquivos de projeto.

---
+++ MuseuAMSG3/InfraStructure/Data/Context/ApplicationDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MuseuAMSG3.Models;
+
+namespace MuseuAMSG3.InfraStructure.Data.Context
+{ 
+
+
+
+
+        public class ApplicationDataContext : DbContext
+{
+    public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Cliente> Cliente { get; set; }
+}
+}
+
---
+++ MuseuAMSG3/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace MuseuAMSG3.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }  
+      
+        public int Telphone { get; set; }
+    }
+}
